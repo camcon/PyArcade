f4e9e388949a99cdf6ce9fa87d4d866f4af2829a
aconte21 <anthonydconte@lewisu.edu>
2014-10-31 11:49:19 -0500

    Tried to add in the good guy shooting

--- arcade/arcade.py
+++ arcade/arcade.py
@@ -9,17 +9,22 @@ height = 300
 setDisplay = pygame.display.set_mode((width, height))
 pygame.display.set_caption('Epic Game')
 
+### Import images here###
 img = pygame.image.load('good_guy.png')
 bguy = pygame.image.load('bad_guy.png')
+gShot = pygame.image.load('gGuyShot.png')
+############################
 imgx = width / 2
 imgy = height - 40 
 
 bGuyY = 0
 bGuyX = (width/2) - 16
 
-movement = 'null'
+key = 'null'
 pixMove = 5
 fps = 30
+gShotVisible = False
+checkShot = False
 
 fpsTime = pygame.time.Clock()
 
@@ -35,7 +40,9 @@ def getKey(direction):# gets the key and returns a direction to be processed in 
             if event.key == K_LEFT:
                 direction = 'left'
             elif event.key == K_RIGHT:
-                direction = 'right'           
+                direction = 'right'
+            elif event.key == K_SPACE:
+                direction = 'shot'
         elif event.type == KEYUP:
             direction = 'null'
     return direction
@@ -55,23 +62,39 @@ def hitTest(bGuyX, bGuyY, imgx, imgy): #not completed obviously
 def bGuyDraw(bguy, bGuyX, bGuyY):
     if bGuyY < height:
         setDisplay.blit(bguy, (bGuyX, bGuyY))
+def drawGShot(imgx, imgy):
+    gShotX = imgx - 16
+    gShotY = imgy + 16
+    gShotVisible = True
+    setDisplay.blit(gShot, (gShotX, gShotY))#the +/- centers the start position
+    return gShotVisible #returns boolean to check and see if the shot is drawn for the main function to see
+def gShotMoveUp(gShotY,pixMove):
+    pixMove = 5        
+    return gShotY - pixMove
 
         
 while True:
     setDisplay.fill(black)
     
-    movement = getKey(movement)    
+    key = getKey(key) 
     
-    if movement == 'right' and imgx < width-36:
-        imgx += pixMove      
+    if key == 'right' and imgx < width-36:
+        imgx += pixMove     
     
-    elif movement == 'left' and imgx > 0:
+    elif key == 'left' and imgx > 0:
         imgx -= pixMove
+    elif key == 'shot':
+        checkShot = True #need to set this to false after 'space' is let go
         
+    drawGShot(imgx, imgy) #checks to see if the shot is drawn    
     playerDraw(img, imgx,imgy)
+    gShotMoveUp(imgy, pixMove)#Doesn't actually move up yet
     
     if(bGuyY > height + 32):
        bGuyX = random.randrange(0, width)
+
+    if (checkShot == True):
+        gShotMoveUp(imgy+16, pixMove)
        
     bGuyDraw(bguy, bGuyX, bGuyY) #Need to Allow random x positions
 
